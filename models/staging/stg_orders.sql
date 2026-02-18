bd067e766cacee01e72d296e62a5f61ed4df4acb
milocarlos90@gmail.com <milocarlos90@gmail.com>
2026-02-18 15:47:09 +0000

    Refactor staging + Standardize naming and add casting

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,13 +1,27 @@
 {{ config(materialized='view') }}
 
-select
-    order_id,
-    user_id,
-    status,
-    gender,
-    created_at,
-    returned_at,
-    shipped_at,
-    delivered_at,
-    num_of_item
-from {{ source('thelook_ecommerce', 'orders') }}
+with source as (
+
+    select *
+    from {{ source('thelook_ecommerce', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        cast(order_id as int64)           as order_id,
+        cast(user_id as int64)            as user_id,
+        cast(status as string)            as status,
+        cast(gender as string)            as gender,
+        cast(created_at as timestamp)     as created_at,
+        cast(returned_at as timestamp)    as returned_at,
+        cast(shipped_at as timestamp)     as shipped_at,
+        cast(delivered_at as timestamp)   as delivered_at,
+        cast(num_of_item as int64)        as number_of_items
+    from source
+
+)
+
+select *
+from renamed
--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,13 +1,27 @@
 {{ config(materialized='view') }}
 
-select
-    id,
-    cost,
-    category,
-    name,
-    brand,
-    retail_price,
-    department,
-    sku,
-    distribution_center_id
-from {{ source('thelook_ecommerce', 'products') }}
+with source as (
+
+    select *
+    from {{ source('thelook_ecommerce', 'products') }}
+
+),
+
+renamed as (
+
+    select
+        cast(id as int64)                       as product_id,
+        cast(cost as numeric)                   as cost,
+        cast(category as string)                as category,
+        cast(name as string)                    as product_name,
+        cast(brand as string)                   as brand,
+        cast(retail_price as numeric)           as retail_price,
+        cast(department as string)              as department,
+        cast(sku as string)                     as sku,
+        cast(distribution_center_id as int64)   as distribution_center_id
+    from source
+
+)
+
+select *
+from renamed
